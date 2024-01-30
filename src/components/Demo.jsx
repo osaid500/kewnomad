79f1e5efa8f7020626db28642436cf725e936f89
osaid <redfirerpk@gmail.com>
2024-01-31 00:42:54 +0300

    Add search form to Demo component

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 
 const Demo = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
   useEffect(() => {
     async function fetchData() {
       try {
@@ -13,10 +15,36 @@ const Demo = () => {
       }
     }
 
-    fetchData();
+    // fetchData();
   }, []);
 
-  return <div>Demo</div>;
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("submitted");
+  }
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="w-full px-10 py-5 text-gray-700 rounded-md focus:outline-none"
+            type="text"
+            placeholder="Search for a job"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            required
+          />
+          <button className="absolute right-0 mr-4 px-4 py-2 rounded-md font-medium bg-emerald-600 text-white transition hover:bg-emerald-700">
+            Find Jobs
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
